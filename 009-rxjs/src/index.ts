5ef1520464d79986b1521f68064d0e8fc1640f75
Shevchenko Dmitry <dmitriy.e.shevchenko@rt.ru>
2021-12-21 10:26:53 +0300

    домашка по rxjs

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -1,9 +1,26 @@
-import {of, from, timer, range} from 'rxjs';
+import {Observable} from "rxjs";
+import axios from "axios";
 
-const o = range(0, 10)
+const requestGit = new Observable((observable) => {
+  axios.get(`https://api.github.com/search/repositories?q=${123}`).then((res) => {
+    observable.next(res)
+  })
+})
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
+const requestJson = new Observable((observable) => {
+  axios.get(`https://jsonplaceholder.typicode.com/posts/${1}`).then((res) => {
+    observable.next(res)
+  })
+})
+
+requestGit.subscribe({
+  next: (value: any) => console.log('Next github:', value),
+  complete: () => console.log('Complete!'),
+  error: (error) => console.log('Error!', error)
+})
+
+requestJson.subscribe({
+  next: (value: any) => console.log('Next :', value),
   complete: () => console.log('Complete!'),
   error: (error) => console.log('Error!', error)
 })
